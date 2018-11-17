92b0ebeb6813e5c7c4759295d9dfcce97c537f8c
David Antoon <david.antoon@hotmail.com>
2018-11-17 02:05:11 +0200

    finish mesh model class

--- Viewer/include/MeshModel.h
+++ Viewer/include/MeshModel.h
@@ -18,42 +18,53 @@ private:
 	std::vector<glm::vec3> normals;
     std::vector<glm::vec2> textures;
     std::vector<Line> boundingBox;
-    
     // scale, rotate, translate and visibilityOptions specified per model
-    std::vector<glm::vec3> scale;
-    std::vector<glm::vec3> rotate;
-    std::vector<glm::vec3> translate;
+    glm::vec3 scale;
+    glm::vec3 rotate;
+    glm::vec3 translate;
     // boundingBox, vertice normals, face normals
-    std::vector<glm::vec3> visibilityOptions;
+    // 0 => hidden, 1 => visible
+    glm::vec3 visibilityOptions;
     
 	glm::mat4x4 worldTransform;
 	glm::vec4 color;
 	std::string modelName;
-    
+
+    void CalculateBoundingBox();
+
+public:
+	MeshModel(const std::vector<Face>& faces, const std::vector<glm::vec3>& vertices, const std::vector<glm::vec3>& normals, const std::vector<glm::vec2>& textures, const std::string& modelName = "");
+	virtual ~MeshModel();
+    const std::string& GetModelName();
+
     //get minimum index value in all the verticses
     // 0 -> x, 1 -> y, 2 -> z
     const float GetMin(int type);
     //get maximum  value in all the vertices
     // 0 -> x, 1 -> y, 2 -> z
     const float GetMax(int type);
-
-	void addLine(glm::vec3& p1, glm::vec3& p2);
-
-public:
-	MeshModel(const std::vector<Face>& faces, const std::vector<glm::vec3>& vertices, const std::vector<glm::vec3>& normals, const std::vector<glm::vec2>& textures, const std::string& modelName = "");
-	virtual ~MeshModel();
-
+    
+    const std::vector<glm::vec3> GetVertices() const;
+    const std::vector<glm::vec3>& GetNormals() const;
+    const std::vector<Face>& GetFaces() const;
+    const glm::vec3 CalculateFaceNormal(Face face) const;
+    const glm::vec3 GetVertexByIndex(int index) const;
+    const std::vector<Line> GetBoundingBox() const;
+    
+    // getter setters
 	void SetWorldTransformation(const glm::mat4x4& worldTransform);
 	const glm::mat4x4& GetWorldTransformation() const;
 
 	const glm::vec4& GetColor() const;
 	void SetColor(const glm::vec4& color);
+    
+    const glm::vec3& GetScale() const;
+    void SetScale(const glm::vec3& scale);
+    
+    const glm::vec3& GetRotate() const;
+    void SetRotate(const glm::vec3& rotate);
+    
+    const glm::vec3& GetTranslate() const;
+    void SetTranslate(const glm::vec3& translate);
 
-	const std::string& GetModelName();
-
-    const glm::vec3 CalculateFaceNormal(Face face);
-    const glm::vec3 GetVertexByIndex(int index);
-    std::vector<Line> CalculateBoundingBox();
-
-	// Add more methods/functionality as needed...
 };
--- Viewer/src/MeshModel.cpp
+++ Viewer/src/MeshModel.cpp
@@ -11,18 +11,133 @@ faces(faces),
 vertices(vertices),
 normals(normals),
 textures(textures),
-worldTransform(glm::mat4x4(1)),
-modelName(modelName),
 scale({ 1,1,1 }),
 rotate({ 0,0,0 }),
 translate({ 0,0,0 }),
-visibilityOptions({ 0,0,0 })
+visibilityOptions({ 0,0,0 }),
+worldTransform(glm::mat4x4(1)),
+modelName(modelName)
 {
+    CalculateBoundingBox();
 }
 
 MeshModel::~MeshModel()
 {
+}
+
+void MeshModel::CalculateBoundingBox()
+{
+    float minX = GetMin(0);
+    float minY = GetMin(1);
+    float minZ = GetMin(2);
     
+    float maxX = GetMax(0);
+    float maxY = GetMax(1);
+    float maxZ = GetMax(2);
+    
+    this->boundingBox.clear();
+    
+    //  square 1
+    //      maxX, maxY, minZ -> maxX, minY, minZ
+    //      maxX, maxY, minZ -> minX, maxY, minZ
+    //      maxX, maxY, minZ -> maxX, minY, minZ
+    //      maxX, maxY, minZ -> minX, maxY, minZ
+    
+    this->boundingBox.push_back(Line(glm::vec3(maxX, maxY, minZ), glm::vec3(maxX, minY, minZ)));
+    this->boundingBox.push_back(Line(glm::vec3(maxX, maxY, minZ), glm::vec3(minX, maxY, minZ)));
+    this->boundingBox.push_back(Line(glm::vec3(maxX, maxY, minZ), glm::vec3(maxX, minY, minZ)));
+    this->boundingBox.push_back(Line(glm::vec3(maxX, maxY, minZ), glm::vec3(minX, maxY, minZ)));
+    
+    //  square 2
+    //      maxX, maxY, maxZ -> maxX, minY, maxZ
+    //      maxX, maxY, maxZ -> minX, maxY, maxZ
+    //      maxX, maxY, maxZ -> maxX, minY, maxZ
+    //      maxX, maxY, maxZ -> minX, maxY, maxZ
+    
+    this->boundingBox.push_back(Line(glm::vec3(maxX, maxY, maxZ), glm::vec3(maxX, minY, maxZ)));
+    this->boundingBox.push_back(Line(glm::vec3(maxX, maxY, maxZ), glm::vec3(minX, maxY, maxZ)));
+    this->boundingBox.push_back(Line(glm::vec3(maxX, maxY, maxZ), glm::vec3(maxX, minY, maxZ)));
+    this->boundingBox.push_back(Line(glm::vec3(maxX, maxY, maxZ), glm::vec3(minX, maxY, maxZ)));
+    
+    // connect square 1 and 2
+    //      minX, minY, minZ -> minX, minY, maxZ
+    //      maxX, minY, minZ -> maxX, minY, maxZ
+    //      minX, maxY, minZ -> minX, maxY, maxZ
+    //      maxX, maxY, minZ -> maxX, maxY, maxZ
+    
+    this->boundingBox.push_back(Line(glm::vec3(minX, minY, minZ), glm::vec3(minX, minY, maxZ)));
+    this->boundingBox.push_back(Line(glm::vec3(maxX, minY, minZ), glm::vec3(maxX, minY, maxZ)));
+    this->boundingBox.push_back(Line(glm::vec3(minX, maxY, minZ), glm::vec3(minX, maxY, maxZ)));
+    this->boundingBox.push_back(Line(glm::vec3(maxX, maxY, minZ), glm::vec3(maxX, maxY, maxZ)));
+}
+
+
+// public methods:
+
+
+const std::string& MeshModel::GetModelName()
+{
+    return modelName;
+}
+
+const float MeshModel::GetMin(int type)
+{
+    std::vector<float> allV;
+    for(auto const& v: this->vertices) {
+        allV.push_back(v[type]);
+    }
+    return *min_element(std::begin(allV), std::end(allV)); // c++11
+}
+
+const float MeshModel::GetMax(int type)
+{
+    std::vector<float> allV;
+    for(auto const& v: this->vertices) {
+        allV.push_back(v[type]);
+    }
+    return *max_element(std::begin(allV), std::end(allV)); // c++11
+}
+
+const std::vector<glm::vec3> MeshModel::GetVertices() const
+{
+    return this->vertices;
+}
+
+const std::vector<glm::vec3>& MeshModel::GetNormals() const
+{
+    return this->normals;
+}
+
+const std::vector<Face>& MeshModel::GetFaces() const
+{
+    return this->faces;
+}
+
+const glm::vec3 MeshModel::CalculateFaceNormal(Face face) const
+{
+    int vXIndex = face.GetVertexByIndex(0);
+    int vYIndex = face.GetVertexByIndex(1);
+    int vZIndex = face.GetVertexByIndex(2);
+    
+    glm::vec3 vertex1 = GetVertexByIndex(vXIndex);
+    glm::vec3 vertex2 = GetVertexByIndex(vYIndex);
+    glm::vec3 vertex3 = GetVertexByIndex(vZIndex);
+    
+    glm::vec3 ver1 = vertex2 - vertex1;
+    glm::vec3 ver2 = vertex3 - vertex1;
+    
+    return cross(ver1, ver2);
+}
+
+const glm::vec3 MeshModel::GetVertexByIndex(int index) const
+{
+    return this->vertices[index];
+}
+
+
+const std::vector<Line> MeshModel::GetBoundingBox() const
+{
+    return this->boundingBox;
 }
 
 void MeshModel::SetWorldTransformation(const glm::mat4x4& worldTransform)
@@ -32,7 +147,7 @@ void MeshModel::SetWorldTransformation(const glm::mat4x4& worldTransform)
 
 const glm::mat4x4& MeshModel::GetWorldTransformation() const
 {
-	return worldTransform;
+	return this->worldTransform;
 }
 
 void MeshModel::SetColor(const glm::vec4& color)
@@ -45,94 +160,32 @@ const glm::vec4& MeshModel::GetColor() const
 	return color;
 }
 
-const std::string& MeshModel::GetModelName()
+const glm::vec3& MeshModel::GetScale() const
 {
-	return modelName;
+    return this->scale;
+}
+void MeshModel::SetScale(const glm::vec3& scale)
+{
+    this->scale = scale;
 }
 
-const glm::vec3 MeshModel::CalculateFaceNormal(Face face){
-    int vXIndex = face.GetVertexByIndex(0);
-    int vYIndex = face.GetVertexByIndex(1);
-    int vZIndex = face.GetVertexByIndex(2);
-
-    glm::vec3 vertex1 = GetVertexByIndex(vXIndex);
-    glm::vec3 vertex2 = GetVertexByIndex(vYIndex);
-    glm::vec3 vertex3 = GetVertexByIndex(vZIndex);
-
-    glm::vec3 ver1 = vertex2 - vertex1;
-    glm::vec3 ver2 = vertex3 - vertex1;
-    
-    return cross(ver1, ver2);
+const glm::vec3& MeshModel::GetRotate() const
+{
+    return this->rotate;
+}
+void MeshModel::SetRotate(const glm::vec3& rotate)
+{
+    this->rotate = rotate;
 }
 
-const glm::vec3 MeshModel::GetVertexByIndex(int index){
-    return this->vertices[index];
+const glm::vec3& MeshModel::GetTranslate() const
+{
+    return this->translate;
 }
-
-const float MeshModel::GetMin(int type){
-    std::vector<float> allV;
-    for(auto const& v: this->vertices) {
-        allV.push_back(v[type]);
-    }
-    return *min_element(std::begin(allV), std::end(allV)); // c++11
+void MeshModel::SetTranslate(const glm::vec3& translate)
+{
+    this->translate = translate;
 }
-
-const float MeshModel::GetMax(int type){
-    std::vector<float> allV;
-    for(auto const& v: this->vertices) {
-        allV.push_back(v[type]);
-    }
-    return *max_element(std::begin(allV), std::end(allV)); // c++11
-}
-
-std::vector<Line> MeshModel::CalculateBoundingBox(){
-    float minX = GetMin(0);
-    float minY = GetMin(1);
-    float minZ = GetMin(2);
-    
-    float maxX = GetMax(0);
-    float maxY = GetMax(1);
-    float maxZ = GetMax(2);
-
-    this->boundingBox.clear();
-    
-    // store drawing lines instead of points
-
-    
-    // TODO: change every line to push_back directly
-    this->boundingBox.push_back(Line(glm::vec3(minX, minY, minZ), glm::vec3(minX, maxY, minZ)));
-    
-	addLine(glm::vec3(minX, minY, minZ), glm::vec3(minX, maxY, minZ));
-	addLine(glm::vec3(minX, minY, minZ), glm::vec3(maxX, minY, minZ));
-	addLine(glm::vec3(minX, minY, minZ), glm::vec3(minX, minY, maxZ));
-
-	addLine(glm::vec3(minX, maxY, minZ), glm::vec3(minX, maxY, maxZ));
-	addLine(glm::vec3(minX, maxY, minZ), glm::vec3(maxX, maxY, minZ));
-
-	addLine(glm::vec3(maxX, minY, minZ), glm::vec3(maxX, maxY, minZ));
-	addLine(glm::vec3(maxX, minY, minZ), glm::vec3(maxX, maxY, maxZ));
-
-	addLine(glm::vec3(maxX, minY, maxZ), glm::vec3(maxX, maxY, maxZ));
-	addLine(glm::vec3(maxX, minY, maxZ), glm::vec3(minX, minY, maxZ));
-
-	addLine(glm::vec3(maxX, maxY, maxZ), glm::vec3(maxX, maxY, minZ));
-	addLine(glm::vec3(maxX, maxY, maxZ), glm::vec3(minX, maxY, maxZ));
-	
-	addLine(glm::vec3(minX, maxY, maxZ), glm::vec3(minX, minY, maxZ));
-
-    return this->boundingBox;
-}
-
-
-
-void MeshModel::addLine(glm::vec3& p1, glm::vec3& p2) {
-	this->boundingBox.push_back(Line(p1, p2));
-}
-
-
-
-
-
 
 
 
