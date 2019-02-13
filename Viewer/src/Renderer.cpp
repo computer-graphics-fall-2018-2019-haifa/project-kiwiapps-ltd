909471c4294ccb97c947f20fbafd2daea4ccd8b3
Ameer Dow <ameerdow@icloud.com>
2019-02-13 16:04:53 +0200

    Update Renderer

--- Viewer/src/Renderer.cpp
+++ Viewer/src/Renderer.cpp
@@ -142,40 +142,68 @@ Renderer::~Renderer()
 // build face vertics from model vertices positions
 // draw bounding box
 void Renderer::DrawModel(std::shared_ptr<MeshModel> model, const std::shared_ptr<Scene>& scene) {
+    glm::mat4 modelMat = model->GetWorldTransformation();
+    
+    // Set the uniform variables
+    colorShader.setUniform("model", modelMat);
+    colorShader.setUniform("material.color", model->color);
+//    colorShader.setUniform("useTexture", model->useTexture);
 
-    // get model faces
-    // get model vertics
-    // get model normals
-    // get model color by checking if is active model -> green else darkGray
-
-    std::vector<Face> faces = model->GetFaces();
-    std::vector<glm::vec3> vertices = model->GetVertices();
-    std::vector<glm::vec3> normals = model->GetNormals();
-
-    // call draw bounding box if requested
-    if (model->boundingBoxVisibility) {
-//        DrawModelBoundingBox(model, transformMatrix);
-    }
-
-    // loop over faces and draw each face by calling the DrawFace function
-        // use the provided transformMatrix when drawing the Faces
-        // check if draw face normals requested
-
-    for (int i = 0; i < faces.size(); i++) {
-        std::vector<glm::vec3> vertices;
-        Face face = faces.at(i);
-        for(int j=0; j<3; j++){
-            vertices.push_back(model->GetVertexByIndex(face.GetVertexByIndex(j) - 1));
-        }
-
-        if (model->faceNoramlVisibility == 1) {
-//            DrawFaceNormal(vertices, transformMatrix);
-        }
-        if(model->verticesNoramlVisibility) {
-
-        }
-//        DrawTriangle(vertices, transformMatrix, model->GetColor());
-    }
+//    if (model->fill) {
+//        // Set the model's texture as the active texture at slot #0
+//        model->BindTexture();
+//
+//        // Drag our model's faces (triangles) in fill mode
+//        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+//        glBindVertexArray(model->GetVAO());
+//        glDrawArrays(GL_TRIANGLES, 0, (GLsizei)model->GetModelVertices().size());
+//        glBindVertexArray(0);
+//
+//        // Unset the model's texture as the active texture at slot #0
+//        model->UnbindTexture();
+//    }
+    
+//    if (model->showWire) {
+        // Drag our model's faces (triangles) in line mode (wireframe)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//        glBindVertexArray(model->GetVAO());
+//        glDrawArrays(GL_TRIANGLES, 0, (GLsizei)model->GetModelVertices().size());
+        glBindVertexArray(0);
+//    }
+    
+//    // get model faces
+//    // get model vertics
+//    // get model normals
+//    // get model color by checking if is active model -> green else darkGray
+//
+//    std::vector<Face> faces = model->GetFaces();
+//    std::vector<glm::vec3> vertices = model->GetVertices();
+//    std::vector<glm::vec3> normals = model->GetNormals();
+//
+//    // call draw bounding box if requested
+//    if (model->boundingBoxVisibility) {
+////        DrawModelBoundingBox(model, transformMatrix);
+//    }
+//
+//    // loop over faces and draw each face by calling the DrawFace function
+//        // use the provided transformMatrix when drawing the Faces
+//        // check if draw face normals requested
+//
+//    for (int i = 0; i < faces.size(); i++) {
+//        std::vector<glm::vec3> vertices;
+//        Face face = faces.at(i);
+//        for(int j=0; j<3; j++){
+//            vertices.push_back(model->GetVertexByIndex(face.GetVertexByIndex(j) - 1));
+//        }
+//
+//        if (model->faceNoramlVisibility == 1) {
+////            DrawFaceNormal(vertices, transformMatrix);
+//        }
+//        if(model->verticesNoramlVisibility) {
+//
+//        }
+////        DrawTriangle(vertices, transformMatrix, model->GetColor());
+//    }
 }
 
 void Renderer::Render(const std::shared_ptr<Scene>& scene, GLFWwindow* window)
